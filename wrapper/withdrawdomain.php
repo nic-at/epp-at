e3cbca94cb42b0970888f17bb85a19b8b712013e
Eugene Surov <eugene.surov@nic.at>
2024-10-11 13:21:09 +0200

    DomainWithdraw implementation.

---
+++ wrapper/withdrawdomain.php
@@ -0,0 +1,123 @@
+#!/usr/bin/env php
+<?php
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use Metaregistrar\EPP\atEppConnection;
+use Metaregistrar\EPP\atEppWithdrawRequest;
+use Metaregistrar\EPP\atEppDomain;
+use Metaregistrar\EPP\eppException;
+
+$opts = [
+    'server:',
+    'domain:',
+    'deletezone',
+    'cltrid:',
+    'logdir:',
+    'logfile',
+    'nossl',
+];
+
+$params = getopt('', $opts);
+
+$serverstring = $params['server'] ?? '';
+$domain = $params['domain'] ?? '';
+$zd = isset($params['deletezone']);
+
+// Ensure required parameters are there
+if (!($serverstring && $domain)) {
+    usage();
+}
+
+// Parsing the server string
+if (!preg_match('/^([\w\d]+):([\S:@]+)@(\S+):(\d+)$/', $serverstring, $matches)) {
+    fwrite(STDERR, "could not parse server string '$serverstring'\n");
+    exit -1;
+}
+[, $username, $password, $hostname, $port] = $matches;
+
+// Check nossl
+if (isset($params['nossl'])) {
+    echo "Warning: --nossl is deprecated and will be ignored ...\n";
+}
+
+try {
+    $logging = false;
+
+    // Check logfile
+    if (isset($params['logfile'])) {
+        fwrite(STDERR, "The option --logfile is deprecated\n");
+        fwrite(STDERR, "use --logdir <directory> instead\n");
+        exit -1;
+    }
+
+    if ($logdir = ($params['logdir'] ?? '')) {
+        $logging = true;
+    }
+
+    $connection = new atEppConnection($logging);
+    $connection->setHostname('ssl://' . $hostname);
+    $connection->setPort($port);
+    $connection->setTimeout(10);
+    $connection->setVerifyPeer(false);
+
+    if ($logging) {
+        $connection->setLogFile(rtrim($logdir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . date('Y-m-d') . '.log');
+    }
+    $connected = $connection->connect();
+    $connection->setUsername($username);
+    $connection->setPassword($password);
+    $logged_in = $connection->login();
+
+    $request = new atEppWithdrawRequest(new atEppDomain($domain), $zd);
+    $response = $connection->request($request);
+
+    if ($response->Success()) {
+        echo 'SUCCESS: ' . $response->getResultCode() . "\n";
+    } else {
+        echo 'FAILED: ' . $response->getResultCode() . "\n";
+        echo 'Domain delete failed: ' . $response->getResultMessage() . "\n\n";
+    }
+
+    check_and_print_conditions($response->getExtensionResult());
+
+    echo "\nATTR: clTRID: " . $response->getClTrId() . "\n";
+    echo "ATTR: svTRID: " . $response->getSvTrId() . "\n";
+
+    $connection->logout();
+    $connection->disconnect();
+
+} catch (eppException $e) {
+    echo $e->getMessage() . "\n";
+    check_and_print_conditions(json_decode($e->getReason(), true));
+    exit -1;
+}
+
+function check_and_print_conditions($conditions) {
+    if (!is_array($conditions)) return false;
+    foreach ($conditions as $condition) {
+        if (!empty($condition['message'])) {
+            echo "Msg: {$condition['message']}\n";
+        }
+        if (!empty($condition['details'])) {
+            echo "Details: {$condition['details']}\n";
+        }
+        echo "\n";
+    }
+}
+
+function usage() {
+    echo <<<END
+
+usage:
+
+ withdrawdomain --server <user>:<pass>@<host>:<port> \
+                --domain <domain>
+                [--deletezone ]
+                [--cltrid <cltrid>]
+                [--logdir <directory>]
+
+END;
+
+    exit -1;
+}
